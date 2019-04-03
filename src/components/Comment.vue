f5569a2a01441d468d6666befb0500021aff7171
changfei <fchang@petromentor.com>
2019-04-03 14:28:20 +0800

    初次提交项目

---
+++ src/components/Comment.vue
@@ -0,0 +1,57 @@
+<template lang='html'>
+  <div id="comment">
+    <button type="button" @click="toogleMap">评论按钮</button>
+    <ul>
+      <li v-for="item in arr" :key="item.text" @click="addItem">{{item.text}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  props: ['krpano'],
+  data () {
+    return {
+      arr: [
+        {text: 1},
+        {text: 2},
+        {text: 3}
+      ]
+    };
+  },
+  methods: {
+    toogleMap(){
+      this.krpano.call(`switch(layer[mapcontainer].visible)`);
+    },
+    addItem(){
+      let text = this.arr[this.arr.length-1].text;
+      this.arr.push({
+        text: ++text
+      })
+    }
+  }
+}
+</script>
+<style lang='css' scoped>
+  #comment {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    z-index: 1;
+  }
+  ul {
+    width: 300px;
+    height: 100px;
+    background-color: rgba(0,0,0,0.5);
+    display: flex;
+  }
+  li {
+    flex: 1;
+    box-sizing: border-box;
+    text-align: center;
+    border: 1px solid #00f;
+    font-size: 50px;
+    color: #f00;
+    line-height: 100px;
+    cursor: pointer;
+  }
+</style>
---
+++ src/components/Vtour.vue
@@ -0,0 +1,43 @@
+<template lang='html'>
+  <div id="panowrap">
+    <comment :krpano="krpano"></comment>
+    <div id="pano"></div>
+  </div>
+</template>
+<script>
+import Comment from './Comment'
+export default {
+  data(){
+    return {
+      krpano: null
+    };
+  },
+  components: {Comment},
+  methods: {
+    krpano_onready_callback(krpano_interface){
+      this.krpano = krpano_interface;
+    }
+  },
+  mounted(){
+    embedpano({
+      swf: "static/vtour/tour.swf",
+      xml: "static/vtour/tour.xml",
+      target: "pano",
+      html5: "auto",
+      mobilescale: "1.0",
+      passQueryParameters: true,
+      onready: this.krpano_onready_callback
+    })
+  }
+}
+</script>
+<style lang='css' scoped>
+  #panowrap {
+    width: 100%;
+    height: 100%;
+  }
+  #pano {
+    width: 100%;
+    height: 100%;
+  }
+</style>
